6f0d03a0437df4720b940ae2b888c86e21611143
Lokesh Kumar <kumar7827lokesh@gmail.com>
2025-08-12 15:22:22 +0530

    28340

--- home/views.py
+++ home/views.py
@@ -9,3 +9,11 @@ def home(request):
 def about(request):
     restaurant_name=Restaurant.objects.first().name
     return render(request,'about.html',{'restaurant_name':restaurant_name})
+
+def custom_404_view(request,exception=None):
+    #try to fetch resturant name(fallback to setting)
+    if resturant.objects.exists():
+        restaurant_name=Restaurant.objects.first().name
+    else:
+        restaurant_name=getattr(settings,"RESTAURANT_NAME",'Our Restaurant')
+    return render(request,"404.html",{"restaurant_name":restaurant_name})
