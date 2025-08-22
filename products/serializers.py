59c2efab8533e88a29e17e40d2ee62775475a645
Lokesh Kumar <kumar7827lokesh@gmail.com>
2025-08-22 22:48:17 +0530

    36732

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item,Feedback
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
+        fields = '__all__'
+
+class FeedbackSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Feedback
         fields = '__all__'
