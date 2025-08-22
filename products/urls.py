dfbfc2b3c6f8eab85b67343acfe115dba6f52c7a
Lokesh Kumar <kumar7827lokesh@gmail.com>
2025-08-22 22:35:41 +0530

    36732

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,4 @@ from .views import *
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('menu/',views.menu_list,name="menu_list"),
-    path('feedback/',views.feedback,name='feedback')
 ]
